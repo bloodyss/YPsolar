da5269cfc0d94939e42d09998f9e6c0dcd823944
lazarevdmitri <lazarevd99@mail.ru>
2024-06-13 13:59:45 +0300

    Update solar_model.py

--- solar_model.py
+++ solar_model.py
@@ -11,16 +11,22 @@ def recalculate_space_objects_positions(space_objects, dt):
 
     Параметры:
 
-    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
+    **space_objects** — список объектов, для которых нужно пересчитать координаты.
     **dt** — шаг по времени
+    
+    star_body - это centre_body, вокруг готорого вращается planet и т.д
     """
 
-    for body in space_objects:
-        Star.calculate_cosmic_body_force(body, space_objects, G)
-        Satelite.calculate_cosmic_body_force(body, space_objects, G)
-    for body in space_objects:
-        if isinstance(body, Satelite):
-            Satelite.move_planet(body, dt)
+    for star_body in space_objects:
+        if isinstance(star_body, Star):
+            star_ID = star_body.ID
+            for planet_body in space_objects:
+                if isinstance(planet_body, Planet) and planet_body.ID /11 == star_ID:
+                    planet_body.rotate_planet_around(star_body, dt)
+                    planet_ID = planet_body.ID
+                    for satelite_body in space_objects:
+                        if isinstance(satelite_body, Satelite) and satelite_body.ID / 11== planet_ID:
+                            satelite_body.rotate_satelite_around(planet_body, dt)
 
 
 if __name__ == "__main__":
